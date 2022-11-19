58b0b471b7ca5b8522006a7736aacb60c96cad4c
Kevin <zhihui.l@outlook.com>
2022-11-19 17:21:06 -0500

    develop facial recognition

--- rpi/modules/camera/camera.py
+++ rpi/modules/camera/camera.py
@@ -1,8 +1,19 @@
 from picamera import PiCamera
 from time import sleep
+import os
 
+# img_path = 'home/zl826/smartdoor/rpi/modules/camera/img/kevin'
+
+# try:
+#     os.mkdir(img_path)
+# except OSError as error:
+#     print(error)
+    
 camera = PiCamera()
 
-camera.start_preview()
-sleep(10)
-camera.stop_preview()
+# camera.start_preview()
+# sleep(10)
+for i in range (1,101):
+    camera.capture("img/jerry/" + str(i) + ".jpg")
+ 
+# camera.stop_preview()
