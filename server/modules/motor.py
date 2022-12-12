cbeb56843c994d44dbcecb1ccf15ddef50318e58
iotcat <i@iotcat.me>
2022-12-12 13:02:57 -0500

    servo improve

--- server/modules/motor.py
+++ server/modules/motor.py
@@ -1,28 +1,29 @@
 import RPi.GPIO as GPIO
 import time as time
 
-
-
 def motor(queue_cmd_to_motor):
  
+
+    PIN = 13
+
+
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(26, GPIO.OUT)
+    GPIO.setup(PIN, GPIO.OUT)
     
-    servo = GPIO.PWM(26,500)
+    servo = GPIO.PWM(PIN,500)
+    servo.start(0)
 
     try:
         while True:
             cmd = queue_cmd_to_motor.get()
             if cmd['type'] == 'OPEN':
-                servo.start(0)
                 servo.ChangeDutyCycle(20)
-                time.sleep(1.5)
-                servo.stop()
+
             if cmd['type'] == 'CLOSE':
-                servo.start(0)
                 servo.ChangeDutyCycle(70)
-                time.sleep(1.5)
-                servo.stop()
+
+            if cmd['type'] == 'STOP':
+                servo.ChangeDutyCycle(0)
     except KeyboardInterrupt:
         servo.stop()
         GPIO.cleanup()
